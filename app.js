a09f17b31e0bfc70bbb9f5b9ad3effe400e26e88
Brandon Groce <brandon.groce@homesite.com>
2016-11-19 23:40:02 -0800

    fixing screwed up api payloads

--- app.js
+++ app.js
@@ -6,50 +6,49 @@ var Mopidy = require('mopidy'),
 
 var app = express();
 var mopidy = new Mopidy({
-    webSocketUrl: "ws://10.0.1.4:6680/mopidy/ws/",
+    webSocketUrl: "ws://raspberrypi.local:6680/mopidy/ws/",
     callingConvention: "by-position-or-by-name"
 });
 
 var version = function(res){
   console.log("checking connection...");
-  mopidy.on("state:online", function () {
-    console.log("Connected to Mopidy");
-    mopidy.getVersion({}).then(function(data){
-      console.log("Version: ", data);
-      res.send(data);
-    })
-    .catch(console.error.bind(console)) // Handle errors here
-    .done();;
-  });
+  console.log("Connected to Mopidy");
+  mopidy.getVersion({}).then(function(data){
+    console.log("Version: ", data);
+    if (res) {res.send(data);}
+  })
+  .catch(console.error.bind(console)) // Handle errors here
+  .done();;
 }
+
+mopidy.on('state:online', function(){
+  version();
+})
 
 app.get('/healthcheck', function (req, res) {
   version(res);
 });
 
 app.get('/history', function(req, res){
-  mopidy.on("state:online", function(){
     mopidy.history.getHistory({}).then(function(data){
       console.log("Current History: ", data);
       res.send(data);
     });
-  });
 });
 
 app.get('/playlists', function(req, res){
-  mopidy.on('state:online', function(){
     mopidy.playlists.getPlaylists().then(function (playlists) {
-        res.send(JSON.parse(playlists));
         console.log(playlists);
+        res.send(playlists);
     })
     .catch(console.error.bind(console)) // Handle errors here
     .done();
-  });
 });
 
 app.get('/setmode/:mode', function(req, res){
   playVideo('https://www.youtube.com/watch?v=tCn-qeMXdwU')
-  queueAndPlay('79kAfkACIjyA4QYyE6j5zQ', 1);
+  console.log("fetching playlists");
+  queueAndPlay('spotify:user:1213350997:playlist:79kAfkACIjyA4QYyE6j5zQ', 1);
 });
 
 var playVideo = function(videourl){
@@ -75,15 +74,33 @@ var queueAndPlay = function (playlistNum, trackNum) {
     playlistNum = playlistNum || 0;
     trackNum = trackNum || 0;
     mopidy.playlists.getPlaylists().then(function (playlists) {
-        var playlist = playlists[playlistNum];
+        console.log("got playlists");
+        var index = 0;
+        for(index; index < playlists.length;index++){
+          if(playlists[index].uri === playlistNum){
+            break;
+          }
+        }
+        var playlist = playlists[index];
+        var tracks = [];
         console.log("Loading playlist:", playlist.name);
-        return mopidy.tracklist.add(playlist.tracks).then(function (tlTracks) {
-            return mopidy.playback.play(tlTracks[trackNum]).then(function () {
+        for (i = 0; i < playlist.tracks.length; i++){
+          tracks.push(playlist.tracks[i].uri);
+          if (i === 10){
+            break;
+          }
+        }
+        console.log(tracks);
+        return mopidy.tracklist.add({"uris":tracks}).then(function (tlTracks) {
+            console.log(tlTracks);
+            console.log("playlist loaded, starting track 1");
+            return mopidy.playback.play({"tl_track":tlTracks[trackNum]}).then(function () {
+                console.log("track started");
                 return mopidy.playback.getCurrentTrack().then(function (track) {
                     console.log("Now playing:", trackDesc(track));
                 });
             });
-        });
+        }).catch(console.error.bind(console));
     })
     .catch(console.error.bind(console)) // Handle errors here
     .done();                            // ...or they'll be thrown here
