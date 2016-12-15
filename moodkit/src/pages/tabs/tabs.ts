6f146076c8b85dc7a72326f28077ea2cc84f6979
brandongroce <brandon@brandongroce.com>
2016-12-14 16:19:22 -0800

    Styling updates

--- moodkit/src/pages/tabs/tabs.ts
+++ moodkit/src/pages/tabs/tabs.ts
@@ -11,8 +11,10 @@ export class TabsPage {
   // this tells the tabs component which Pages
   // should be each tab's root Page
   tab1Root: any = HomePage;
-  tab2Root: any = AboutPage;
-  tab3Root: any = ContactPage;
+  tab2Root: any = HomePage;
+  tab3Root: any = HomePage;
+  tab4Root: any = HomePage;
+  tab5Root: any = HomePage;
 
   constructor() {
 
